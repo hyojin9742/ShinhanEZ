6dd83d1873e9905915ef090ad4b0f9efdfa19407
JHJin <hyojin9742@gmail.com>
2026-01-14 14:37:10 +0900

    mapper 메서드 수정

--- ShinhanEZ/src/main/java/com/shinhanez/admin/mapper/ContractMapper.java
+++ ShinhanEZ/src/main/java/com/shinhanez/admin/mapper/ContractMapper.java
@@ -10,13 +10,13 @@ import com.shinhanez.admin.domain.Contracts;
 @Mapper
 public interface ContractMapper {
 	// 계약 목록 조회
-	public List<Contracts> allContractList(@Param("startRow") int startRow, @Param("endRow") int endRow);
+	public List<Contracts> selectAllContractList(@Param("startRow") int startRow, @Param("endRow") int endRow);
 	// 계약 전체 건수
 	public int countAllContracts();
 	// 계약 단건 조회
-	public Contracts getContract(Integer ctrId);
+	public Contracts selectOneContract(Integer ctrId);
 	// 계약 등록
-	public int registerContract(Contracts ctr);
+	public int insertContract(Contracts ctr);
 	// 계약 수정
-	public int reviseContract(Contracts ctr);
+	public int updateContract(Contracts ctr);
 }
