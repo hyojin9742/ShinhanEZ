94a639bdf5bd495ca941fb4124719f1c6aaa0a4e
JHJin <hyojin9742@gmail.com>
2026-01-15 10:17:10 +0900

    서비스 테스트 완료

---
+++ ShinhanEZ/src/main/java/com/shinhanez/admin/service/ContractService.java
@@ -0,0 +1,16 @@
+package com.shinhanez.admin.service;
+
+import java.util.Map;
+
+import com.shinhanez.admin.domain.Contracts;
+
+public interface ContractService {
+	// 계약 목록 조회
+	public Map<String, Object> readAllList(int pageNum, int pageSize);
+	// 계약 단건 조회
+	public Contracts readOneContract(Integer ctrId);
+	// 계약 등록
+	public int registerContract(Contracts ctr);
+	// 계약 수정
+	public int updateContract(Contracts ctr);
+}
